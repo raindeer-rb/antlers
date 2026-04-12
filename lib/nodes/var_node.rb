156a8e2c06549e2410585f3629d3795f15025be4
maedi <maediprichard@gmail.com>
2026-04-12 20:13:54 +1000

    Escape HTML even when handling name error

--- lib/nodes/var_node.rb
+++ lib/nodes/var_node.rb
@@ -14,21 +14,27 @@ module Antlers
       @value = value
     end
 
+    def render(current_binding: nil, parent_binding: nil, slot_node: nil, namespace: nil)
+      ERB::Util.html_escape(evaluate_value(current_binding))
+    end
+
+    private
+
     # A variable is deliberately limited in what it can represent.
     #  1. An instance/local variable
     #  2. A method call
     #  3. A static string
-    def render(current_binding: nil, parent_binding: nil, slot_node: nil, namespace: nil)
+    def evaluate_value(current_binding)
       if current_binding
         return current_binding.receiver.instance_variable_get(@value) if @value.start_with?('@')
         return current_binding.local_variable_get(@value) if current_binding.local_variable_defined?(@value)
         return current_binding.receiver.send(@value.to_sym) if current_binding.receiver.respond_to?(@value.to_sym)
       end
 
-      # Variables should not contain HTML.
-      ERB::Util.html_escape(@value)
-    rescue StandardError => e
-      value
+      @value
+    rescue NameError => e
+      # TODO: Must be a better way to handle variables input as literal strings.
+      @value
     end
   end
 end
