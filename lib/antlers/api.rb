a7cff8b4641232da2891ce57ed8b9acd933ad848
maedi <maediprichard@gmail.com>
2026-08-30 22:23:14 +1000

    Release V1 API

---
+++ lib/antlers/api.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative '../elements'
+require_relative '../interfaces/lexeme'
+require_relative '../interfaces/branch_node'
+require_relative '../interfaces/leaf_node'
+require_relative '../modules/props'
+require_relative '../modules/variables'
+
+# Usage: require 'antlers/api'
