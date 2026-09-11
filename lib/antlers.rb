66cbad5811630b1a089dc9df229637ec6a227457
maedi <maediprichard@gmail.com>
2026-09-11 17:08:07 +1000

    Requires should now be required by antlers/api

--- lib/antlers.rb
+++ lib/antlers.rb
@@ -4,10 +4,6 @@ require_relative 'antlers/elements'
 require_relative 'antlers/lexer'
 require_relative 'antlers/parser'
 
-# These requires allow downstream libraries to build their own antlers nodes.
-require_relative 'interfaces/branch_node'
-require_relative 'interfaces/leaf_node'
-
 module Antlers
   DEFAULT_ELEMENTS = [:root, :html, :form, :for, :if, :prop, :slot, :yield, :var]
 
